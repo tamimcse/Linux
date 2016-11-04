db98ff012f3c3f0cf4552e85f93a2479df5adb42
tamimcse <tamim@csebuet.org>
2016-11-04 13:34:34 -0400

    Netlink kernel added

--- net/ipv4/tcp_mf.c
+++ net/ipv4/tcp_mf.c
@@ -1,64 +1,71 @@
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <net/sock.h> 
+#include <linux/netlink.h>
+#include <linux/skbuff.h> 
+#define NETLINK_USER 31
 
-#include <net/sock.h>
-#include <net/netlink.h>
+struct sock *nl_sk = NULL;
 
-#define MSG_TCP_MF (0x10 + 2)  // + 2 is arbitrary. same value for kern/usr
-
-static struct sock *tcp_mf_nl_sock;
-
-DEFINE_MUTEX(tcp_mf_mutex);
-
-static int
-rcv_msg(struct sk_buff *skb, struct nlmsghdr *nlh)
+static void hello_nl_recv_msg(struct sk_buff *skb)
 {
-    int type;
-    char *data;
 
-    type = nlh->nlmsg_type;
-    if (type != MSG_TCP_MF) {
-        printk("%s: expect %#x got %#x\n", __func__, MSG_TCP_MF, type);
-        return -EINVAL;
+    struct nlmsghdr *nlh;
+    int pid;
+    struct sk_buff *skb_out;
+    int msg_size;
+    char *msg = "Hello from kernel";
+    int res;
+
+    printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
+
+    msg_size = strlen(msg);
+
+    nlh = (struct nlmsghdr *)skb->data;
+    printk(KERN_INFO "Netlink received msg payload:%s\n", (char *)nlmsg_data(nlh));
+    pid = nlh->nlmsg_pid; /*pid of sending process */
+
+    skb_out = nlmsg_new(msg_size, 0);
+    if (!skb_out) {
+        printk(KERN_ERR "Failed to allocate new skb\n");
+        return;
     }
 
-    data = NLMSG_DATA(nlh);
-    printk("%s: %02x %02x %02x %02x %02x %02x %02x %02x\n", __func__,
-            data[0], data[1], data[2], data[3],
-            data[4], data[5], data[6], data[7]);
-    return 0;
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
+    strncpy(nlmsg_data(nlh), msg, msg_size);
+
+    res = nlmsg_unicast(nl_sk, skb_out, pid);
+    if (res < 0)
+        printk(KERN_INFO "Error while sending bak to user\n");
 }
 
-static void
-nl_rcv_msg(struct sk_buff *skb)
+static int __init hello_init(void)
 {
-    mutex_lock(&tcp_mf_mutex);
-    netlink_rcv_skb(skb, &rcv_msg);
-    mutex_unlock(&tcp_mf_mutex);
-}
 
-static int
-tcp_mf_init(void)
-{
+    printk("Entering: %s\n", __FUNCTION__);
+    //nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, 0, hello_nl_recv_msg, NULL, THIS_MODULE);
     struct netlink_kernel_cfg cfg = {
-        .input = nl_rcv_msg,
-    };    
-    tcp_mf_nl_sock = netlink_kernel_create(&init_net, NETLINK_USERSOCK, &cfg);    
-    if (!tcp_mf_nl_sock) {
-        printk(KERN_ERR "%s: receive handler registration failed\n", __func__);
-        return -ENOMEM;
+        .input = hello_nl_recv_msg,
+    };
+
+    nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
+    if (!nl_sk) {
+        printk(KERN_ALERT "Error creating socket.\n");
+        return -10;
     }
+    printk(KERN_INFO "Created NETLINK_USER socket.\n");
 
     return 0;
 }
 
-static void
-tcp_mf_exit(void)
+static void __exit hello_exit(void)
 {
-    if (tcp_mf_nl_sock) {
-        netlink_kernel_release(tcp_mf_nl_sock);
-    }
+
+    printk(KERN_INFO "exiting hello module\n");
+    netlink_kernel_release(nl_sk);
 }
 
-module_init(tcp_mf_init);
-module_exit(tcp_mf_exit);
+module_init(hello_init); 
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
