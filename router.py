a3cdfa5d015bddd805daea7d57774f95030fe2df
tamimcse <tamim@csebuet.org>
2016-11-17 17:07:52 -0500

    added 3+3 hosts in mininet

--- router.py
+++ router.py
@@ -17,6 +17,9 @@ already built into Linux.
 #       172.16.103.0/24         172.16.10.0/24         172.16.104.0./24
 #  h3 ------------------- r1 ------------------ r2------- -------------h4
 #    .1                .2   .2               .3   .2                 .1
+#       172.16.105.0/24         172.16.10.0/24         172.16.106.0./24
+#  h4 ------------------- r1 ------------------ r2------- -------------h4
+#    .1                .2   .2               .3   .2                 .1
 
 ##############################################################################
 """
@@ -52,16 +55,21 @@ class NetworkTopo( Topo ):
         h3 = self.addHost( 'h3', ip='172.16.103.1/24', defaultRoute='via 172.16.103.2' )
         h4 = self.addHost( 'h4', ip='172.16.104.1/24', defaultRoute='via 172.16.104.2' )
 
+        h5 = self.addHost( 'h5', ip='172.16.105.1/24', defaultRoute='via 172.16.105.2' )
+        h6 = self.addHost( 'h6', ip='172.16.106.1/24', defaultRoute='via 172.16.106.2' )
+
         r1 = self.addNode( 'r1', cls=LinuxRouter, ip='172.16.101.2/24' )
         r2 = self.addNode( 'r2', cls=LinuxRouter, ip='172.16.102.2/24' )
 
-        self.addLink( r1, r2, intfName1='r1-eth1', params1={ 'ip' : '172.16.10.2/24' }, intfName2='r2-eth1', params2={ 'ip' : '172.16.10.3/24' } )
-
         self.addLink( h1, r1, intfName2='r1-eth2', params2={ 'ip' : '172.16.101.2/24' } )
         self.addLink( h2, r2, intfName2='r2-eth2', params2={ 'ip' : '172.16.102.2/24' } )
-
+#       Don't move the line. It doesn't work for some reason
+        self.addLink( r1, r2, intfName1='r1-eth1', params1={ 'ip' : '172.16.10.2/24' }, intfName2='r2-eth1', params2={ 'ip' : '172.16.10.3/24' } )
         self.addLink( h3, r1, intfName2='r1-eth3', params2={ 'ip' : '172.16.103.2/24' } )
         self.addLink( h4, r2, intfName2='r2-eth3', params2={ 'ip' : '172.16.104.2/24' } )
+
+        self.addLink( h5, r1, intfName2='r1-eth4', params2={ 'ip' : '172.16.105.2/24' } )
+        self.addLink( h6, r2, intfName2='r2-eth4', params2={ 'ip' : '172.16.106.2/24' } )
 
 def run():
     "Test linux router"
@@ -72,8 +80,10 @@ def run():
 #    net[ 'r1' ].cmd( 'ip neigh add 172.16.10.3 lladdr 2e:a9:cf:14:b4:6a dev r1-eth1' )
     net[ 'r1' ].cmd( 'ip route add 172.16.102/24 nexthop via 172.16.10.3' )
     net[ 'r1' ].cmd( 'ip route add 172.16.104/24 nexthop via 172.16.10.3' )
+    net[ 'r1' ].cmd( 'ip route add 172.16.106/24 nexthop via 172.16.10.3' )
     net[ 'r2' ].cmd( 'ip route add 172.16.101/24 nexthop via 172.16.10.2' )
     net[ 'r2' ].cmd( 'ip route add 172.16.103/24 nexthop via 172.16.10.2' )
+    net[ 'r2' ].cmd( 'ip route add 172.16.105/24 nexthop via 172.16.10.2' )
 
     info( '*** Routing Table on Router:\n' )
     info( net[ 'r1' ].cmd( 'route' ) )
