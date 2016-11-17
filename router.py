6ac8a77558446526df50b198832ed229657aa471
tamimcse <tamim@csebuet.org>
2016-11-17 17:00:11 -0500

    Added two more hosts in Mininet.

--- router.py
+++ router.py
@@ -13,6 +13,11 @@ already built into Linux.
 #       172.16.101.0/24         172.16.10.0/24         172.16.102.0./24
 #  h1 ------------------- r1 ------------------ r2------- -------------h2
 #    .1                .2   .2               .3   .2                 .1
+#
+#       172.16.103.0/24         172.16.10.0/24         172.16.104.0./24
+#  h3 ------------------- r1 ------------------ r2------- -------------h4
+#    .1                .2   .2               .3   .2                 .1
+
 ##############################################################################
 """
 
@@ -44,12 +49,19 @@ class NetworkTopo( Topo ):
         h1 = self.addHost( 'h1', ip='172.16.101.1/24', defaultRoute='via 172.16.101.2' )
         h2 = self.addHost( 'h2', ip='172.16.102.1/24', defaultRoute='via 172.16.102.2' )
 
+        h3 = self.addHost( 'h3', ip='172.16.103.1/24', defaultRoute='via 172.16.103.2' )
+        h4 = self.addHost( 'h4', ip='172.16.104.1/24', defaultRoute='via 172.16.104.2' )
+
         r1 = self.addNode( 'r1', cls=LinuxRouter, ip='172.16.101.2/24' )
         r2 = self.addNode( 'r2', cls=LinuxRouter, ip='172.16.102.2/24' )
 
-        self.addLink( h1, r1, intfName2='r1-eth0', params2={ 'ip' : '172.16.101.2/24' } )
-        self.addLink( h2, r2, intfName2='r2-eth0', params2={ 'ip' : '172.16.102.2/24' } )
         self.addLink( r1, r2, intfName1='r1-eth1', params1={ 'ip' : '172.16.10.2/24' }, intfName2='r2-eth1', params2={ 'ip' : '172.16.10.3/24' } )
+
+        self.addLink( h1, r1, intfName2='r1-eth2', params2={ 'ip' : '172.16.101.2/24' } )
+        self.addLink( h2, r2, intfName2='r2-eth2', params2={ 'ip' : '172.16.102.2/24' } )
+
+        self.addLink( h3, r1, intfName2='r1-eth3', params2={ 'ip' : '172.16.103.2/24' } )
+        self.addLink( h4, r2, intfName2='r2-eth3', params2={ 'ip' : '172.16.104.2/24' } )
 
 def run():
     "Test linux router"
@@ -59,7 +71,9 @@ def run():
     info( '*** Configuring routers:\n' )
 #    net[ 'r1' ].cmd( 'ip neigh add 172.16.10.3 lladdr 2e:a9:cf:14:b4:6a dev r1-eth1' )
     net[ 'r1' ].cmd( 'ip route add 172.16.102/24 nexthop via 172.16.10.3' )
+    net[ 'r1' ].cmd( 'ip route add 172.16.104/24 nexthop via 172.16.10.3' )
     net[ 'r2' ].cmd( 'ip route add 172.16.101/24 nexthop via 172.16.10.2' )
+    net[ 'r2' ].cmd( 'ip route add 172.16.103/24 nexthop via 172.16.10.2' )
 
     info( '*** Routing Table on Router:\n' )
     info( net[ 'r1' ].cmd( 'route' ) )
