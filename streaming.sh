9dc66b6a2b743c61fb74a3bc7dfd2b6ac94cbfe0
tamimcse <tamim@csebuet.org>
2016-11-28 21:45:07 -0500

    .

--- streaming.sh
+++ streaming.sh
@@ -1,1 +1,2 @@
 gst-launch-1.0  tcpclientsrc host=$1 port=8554 ! h264parse ! avdec_h264 ! xvimagesink &
+#gst-launch-1.0  tcpclientsrc host=$1 port=8554 ! h264parse ! avdec_h264 ! mpegtsmux ! filesink location=test.ts &
