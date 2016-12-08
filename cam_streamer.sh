348e6f42ab5f3b9d8562c94522a9f9b9351548f7
tamimcse <tamim@csebuet.org>
2016-12-07 23:00:31 -0500

    .

--- cam_streamer.sh
+++ cam_streamer.sh
@@ -1,2 +1,2 @@
-sudo gst-launch-1.0 v4l2src device=/dev/video0 num-buffers=3000 ! video/x-raw, framerate=30/1, width=1280, height=720 ! x264enc bitrate=2048 ! tcpserversink host=$1 port=8554 &
+sudo gst-launch-1.0 v4l2src device=/dev/video0 num-buffers=900 ! video/x-raw, framerate=30/1, width=1280, height=720 ! x264enc bitrate=2048 ! tcpserversink host=$1 port=8554 &
 
--- mininet.sh
+++ mininet.sh
@@ -1,7 +1,10 @@
-sudo modprobe -r tcp_probe
-sudo modprobe tcp_probe port=0 full=1 bufsize=128
+sudo rm -f trace.log &&
+sudo modprobe -r tcp_probe &&
+sudo modprobe tcp_probe port=0 full=1 bufsize=128 &&
+#Single & because it's a background task
 sudo cat /proc/net/tcpprobe > trace.log &
-TCPCAP=$!
+TCPCAP=$! &&
+echo $TCPCAP &&
 sudo dmesg -c &&
-sudo python router.py
+sudo python router.py &&
 kill $TCPCAP
