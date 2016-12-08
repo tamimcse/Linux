604f903a6a1418eaef6132a126862b2362a61e9b
tamimcse <tamim@csebuet.org>
2016-12-08 14:58:15 -0500

    split the log based on hosts

--- mininet.sh
+++ mininet.sh
@@ -8,3 +8,6 @@ echo $TCPCAP &&
 sudo dmesg -c &&
 sudo python router.py &&
 sudo kill $TCPCAP
+cat trace.log | grep  '172.16.101.1:8554 172.16' > h1.log
+cat trace.log | grep  '172.16.103.1:8554 172.16' > h3.log
+cat trace.log | grep  '172.16.105.1:8554 172.16' > h5.log
