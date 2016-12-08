054f683b1e2473d803ab1124abd49063d0028a8d
tamimcse <tamim@csebuet.org>
2016-12-07 22:35:08 -0500

    Hooked the tcp_probe to mininet

--- mininet.sh
+++ mininet.sh
@@ -1,2 +1,7 @@
+sudo modprobe -r tcp_probe
+sudo modprobe tcp_probe port=0 full=1 bufsize=128
+sudo cat /proc/net/tcpprobe > trace.log &
+TCPCAP=$!
 sudo dmesg -c &&
 sudo python router.py
+kill $TCPCAP
