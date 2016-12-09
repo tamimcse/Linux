1ca881e0eb59dfa82e9de411dae6cad6026f747f
tamimcse <tamim@csebuet.org>
2016-12-08 21:02:34 -0500

    automated tcp_probe deletion properly. Now it working properly. Be caustious..

--- mininet.sh
+++ mininet.sh
@@ -1,20 +1,28 @@
-sudo modprobe -r tcp_probe &&
-#trace all ports. write log whenever receive an acknowledgement
-sudo modprobe tcp_probe port=0 full=1 bufsize=128 &&
+sudo rm *.png
+sudo rm logs/*.log
+sudo chmod a+rwx -R ../Linux &&
+cd logs &&
+sudo touch trace.log &&
+sudo touch old.log &&
+sudo chmod a+rwx *.log &&
+cd .. &&
 sudo dmesg -c > logs/old.log &&
+#trace all ports. write log whenever receive an acknowledgement
+sudo modprobe tcp_probe port=0 full=0 bufsize=128 &&
 #Single & because it's a background task
+sudo chmod a+rwx /proc/net/tcpprobe &&
 sudo cat /proc/net/tcpprobe > ./logs/trace.log &
 TCPCAP=$! &&
 echo $TCPCAP &&
 sudo python router.py &&
-sudo kill $TCPCAP &&
 cd logs &&
 sudo cat trace.log | grep  '172.16.101.1:8554 172.16' > h1.log &&
 sudo cat trace.log | grep  '172.16.103.1:8554 172.16' > h3.log &&
 sudo cat trace.log | grep  '172.16.105.1:8554 172.16' > h5.log &&
 cd .. &&
-gnuplot -c tcp.plt ./logs/h1.log h1
-#gnuplot -c tcp.plt ./logs/h3.log h3
-#gnuplot -c tcp.plt ./logs/h5.log h5
-
+sudo gnuplot -c tcp.plt ./logs/h3.log h3 &&
+sudo gnuplot -c tcp.plt ./logs/h5.log h5 &&
+sudo gnuplot -c tcp.plt ./logs/h1.log h1 &&
+sudo kill $TCPCAP &&
+sudo modprobe -r tcp_probe
 
