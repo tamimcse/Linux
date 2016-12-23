5ee5056991af0385956612e320a6f6bcb8696389
tamimcse <tamim@csebuet.org>
2016-12-23 16:42:14 -0500

    unload module

--- module.sh
+++ module.sh
@@ -1,4 +1,6 @@
-for pid in $(sudo lsof | grep tcpprobe | awk '{print $2}') ; do sudo kill $pid ; done &&
-sudo modprobe -r tcp_probe &&
+mod=sch_tbf
+echo $mod 
+for pid in $(sudo lsof | grep $mod | awk '{print $2}') ; do sudo kill $pid ; done &&
+sudo modprobe -r $mod &&
 sudo make modules_install 
-#sudo modprobe tcp_probe 
+sudo modprobe $mod 
