805e4360fbf06990394a09ca602c44a7b3181637
tamimcse <tamim@csebuet.org>
2017-01-14 13:50:49 -0500

    flip im flag programmatically

--- turnim.sh
+++ turnim.sh
@@ -1,1 +1,9 @@
-sudo sysctl -w net.ipv4.tcp_mf=1
+is_mf=$(cat /proc/sys/net/ipv4/tcp_mf)
+echo $is_mf
+if [ $is_mf -eq '1' ]
+then
+    sudo sysctl net.ipv4.tcp_mf=0
+else
+    sudo sysctl net.ipv4.tcp_mf=1
+fi
+
