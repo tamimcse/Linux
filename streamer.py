5fb6aa4d57c1a4ef9c0f8b00cbabab35f0c6da30
tamimcse <tamim@csebuet.org>
2017-01-15 14:29:53 -0500

    Switched to python based streamer

---
+++ streamer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os
+import gi
+import sys, getopt
+gi.require_version('Gst', '1.0')
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gst, Gtk
+
+host = sys.argv[1]
+pipeline = 'videotestsrc pattern=snow num-buffers=1800 ! video/x-raw, framerate=30/1, width=512, height=340 ! x264enc bitrate=512 ! tcpserversink host=' + host + ' port=8554'
+Gst.init(None)
+pipeline = Gst.parse_launch(pipeline)
+pipeline.set_state(Gst.State.PLAYING)
+Gtk.main()
+
