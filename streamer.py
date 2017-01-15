f32f0883296cb5ce6a9bc7bbceae18b2a77ef84c
tamimcse <tamim@csebuet.org>
2017-01-15 17:22:58 -0500

    created gstreamer pipeline programatically

--- streamer.py
+++ streamer.py
@@ -8,15 +8,43 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gst, GObject, Gtk
 
 host = sys.argv[1]
-pipeline = 'videotestsrc pattern=snow num-buffers=1800 ! video/x-raw, framerate=30/1, width=512, height=340 ! x264enc bitrate=512 ! tcpserversink host=' + host + ' port=8554'
+#pipeline_str = 'videotestsrc pattern=snow num-buffers=1800 ! video/x-raw, framerate=30/1, width=512, height=340 ! x264enc bitrate=512 ! tcpserversink host=' + host + ' port=8554'
+#pipeline = Gst.parse_launch(pipeline)
 
 GObject.threads_init()
 Gst.init(None)
 
-#pipeline = gst.Pipeline() 
+pipeline = Gst.Pipeline() 
 
-pipeline = Gst.parse_launch(pipeline)
+src = Gst.ElementFactory.make("videotestsrc","src")
+src.set_property('pattern', 'snow')
+src.set_property('num-buffers', 1800)
+
+cfilter = Gst.ElementFactory.make("capsfilter", "cfilter")
+caps = Gst.Caps.from_string("video/x-raw, framerate=30/1, width=512, height=340")
+cfilter.set_property("caps", caps)
+
+enc = Gst.ElementFactory.make("x264enc","enc")
+enc.set_property('bitrate', 512)
+
+svr = Gst.ElementFactory.make("tcpserversink","svr")
+svr.set_property('host', host)
+svr.set_property('port', 8554)
+
+pipeline.add(src)
+pipeline.add(cfilter)
+pipeline.add(enc)
+pipeline.add(svr)
+
+src.link(cfilter)
+cfilter.link(enc)
+enc.link(svr)
+
+if (not pipeline or not src or not cfilter or not enc or not svr):
+    print('Not all elements could be created.')
+    exit(-1)
+
+#print "Hello"
 pipeline.set_state(Gst.State.PLAYING)
-
 Gtk.main()
 
