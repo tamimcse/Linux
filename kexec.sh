cda67eba2d369564b6caf1e5dce111a63c07862b
tamimcse <tamim@csebuet.org>
2017-02-03 20:30:20 -0500

    install kernel header

--- kexec.sh
+++ kexec.sh
@@ -1,3 +1,4 @@
 sudo make modules_install install
+sudo make headers_install ARCH=i386 INSTALL_HDR_PATH=/usr
 sudo kexec -l /boot/vmlinuz-4.9.0-rc3+ --initrd=/boot/initrd.img-4.9.0-rc3+ --reuse-cmdline
 sudo kexec -e
