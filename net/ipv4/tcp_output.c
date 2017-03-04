44e69d5ca9f22c4f0c67e547a40a91aa388ba072
tamimcse <tamim@csebuet.org>
2017-03-04 16:17:46 -0500

    Set the initial rate to 340kbps

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -985,8 +985,8 @@ static int tcp_transmit_skb(struct sock *sk, struct sk_buff *skb, int clone_it,
             {                
                 tp->mf_cookie_req->feedback_thput
                         = tp->mf_cookie_req->req_thput
-                        = 1024/8;
-                tp->mf_cookie_req->cur_thput = 512/8;
+                        = 340/8;
+                tp->mf_cookie_req->cur_thput = 340/8;
                 tp->mf_cookie_req->prop_delay_est = 140;
                 tp->mf_cookie_req->len = TCPOLEN_MF_ALIGNED;                     
             }
