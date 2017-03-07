6a712ea917b2e0dde6fbb5f887d2f844c6ab137d
tamimcse <tamim@csebuet.org>
2017-03-06 22:09:25 -0500

    Updated the window and pacing rate calculation

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -3323,11 +3323,14 @@ static void tcp_cong_control(struct sock *sk, u32 ack, u32 acked_sacked,
         if(likely(sysctl_tcp_mf) && tp->rx_opt.mf_ok)
         {
             //MF TCP feedback is in KB and everything in kernel is in Byte
-            int wnd = (((tp->mf_cookie_req->feedback_thput * 1024) * (tp->srtt_us/USEC_PER_SEC)) / tp->mss_cache);
+//            int wnd = (((tp->mf_cookie_req->feedback_thput * 1024) * ((tp->srtt_us >> 3) /USEC_PER_SEC)) / tp->mss_cache);
+            
+            //calculated based on tcp_update_pacing_rate()
+            int wnd = ((tp->mf_cookie_req->feedback_thput * 1024) * tp->srtt_us) / (tp->mss_cache * ((USEC_PER_SEC/100) << 3));
             tp->snd_cwnd = wnd;
             pr_info("Feedback= %d RTT= %d MSS= %d Cwnd= %d on ", 
                     tp->mf_cookie_req->feedback_thput, tp->srtt_us, tp->mss_cache, tp->snd_cwnd);
-            tcp_update_pacing_rate(sk);
+            ACCESS_ONCE(sk->sk_pacing_rate) = tp->mf_cookie_req->feedback_thput * 1024;
             return;
         }
 
