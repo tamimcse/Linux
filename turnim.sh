ac972570711809b4183d26868ead4c625db64388
tamimcse <tamim@csebuet.org>
2017-03-08 00:39:28 -0500

    Set Congestion control to BBR

--- turnim.sh
+++ turnim.sh
@@ -3,7 +3,9 @@ echo $is_mf
 if [ $is_mf -eq '1' ]
 then
     sudo sysctl net.ipv4.tcp_mf=0
+    sudo sysctl net.ipv4.tcp_congestion_control=bbr
 else
     sudo sysctl net.ipv4.tcp_mf=1
+    sudo sysctl net.ipv4.tcp_congestion_control=cubic
 fi
 
