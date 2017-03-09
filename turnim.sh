e856d7623cdcb390f542d1654ad49b28b0186605
tamimcse <tamim@csebuet.org>
2017-03-09 15:04:13 -0500

    Added CDG to script

--- turnim.sh
+++ turnim.sh
@@ -1,8 +1,14 @@
 is_mf=$(cat /proc/sys/net/ipv4/tcp_mf)
+cong=$(cat /proc/sys/net/ipv4/tcp_congestion_control)
 echo $is_mf
+echo $cong
+
 if [ $is_mf -eq '1' ]
 then
     sudo sysctl net.ipv4.tcp_mf=0
+    sudo sysctl net.ipv4.tcp_congestion_control=cdg
+elif [ $cong = "cdg" ]
+then
     sudo sysctl net.ipv4.tcp_congestion_control=bbr
 else
     sudo sysctl net.ipv4.tcp_mf=1
