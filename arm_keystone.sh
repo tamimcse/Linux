165095c67c81cc6418cb464dd6258d7d085d055e
tamimcse <tamim@csebuet.org>
2017-07-20 21:28:55 -0400

    added qemu commands

--- arm_keystone.sh
+++ arm_keystone.sh
@@ -1,4 +1,6 @@
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- keystone_defconfig;
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  zImage -j8
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  zImage -j8
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- UIMAGE_LOADADDR=0x8000 uImage -j8
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- keystone-k2hk-evm.dtb
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  modules
+#sudo qemu-system-arm -M vexpress-a15 -m 256M -kernel arch/arm/boot/uImage -initrd rootfs
--- arm_zync.sh
+++ arm_zync.sh
@@ -3,3 +3,6 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- xilinx_zynq_defconfig;
 #for Qemu
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- xilinx_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  UIMAGE_LOADADDR=0x8000 uImage -j8
+
+#./qemu-img create -f qcow2 test.qcow2 16G
+#sudo x86_64-softmmu/qemu-system-x86_64 -m 1024 -enable-kvm -drive if=virtio,file=test.qcow2,cache=none -cdrom ubuntu-16.04.2-desktop-amd64.iso
