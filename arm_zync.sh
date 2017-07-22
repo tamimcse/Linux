48fb768f888070256fea37cc094ec15e0a5660cd
tamimcse <tamim@csebuet.org>
2017-07-22 18:32:49 -0400

    x86 and versatilepb emulation from vmlinuz and initrd.

--- arm_zync.sh
+++ arm_zync.sh
@@ -8,9 +8,11 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  UIMAGE_LOADADDR=0x8000 uImage -j
 #X86 emulation
 #./qemu-img create -f qcow2 test.qcow2 16G
 #sudo x86_64-softmmu/qemu-system-x86_64 -m 1024 -enable-kvm -drive if=virtio,file=test.qcow2,cache=none -cdrom ubuntu-16.04.2-desktop-amd64.iso
+#sudo x86_64-softmmu/qemu-system-x86_64 -m 1024 -enable-kvm -drive if=virtio,file=test.qcow2,cache=none -kernel vmlinuz-4.11.0+ -initrd initrd.img-4.11.0+ -append "root=/dev/ram"  -no-reboot
 
 
 #ARM emulation
-#./configure --target-list=arm-softmmu,arm-linux-user && make -j8
+#./configure --target-list=arm-softmmu,aarch64-softmmu,arm-linux-user --audio-drv-list=alsa,sdl,oss
 #./qemu-img create -f raw armdisk.img 8G
 #sudo arm-softmmu/qemu-system-arm -m 1024M -sd armdisk.img -M vexpress-a9 -cpu cortex-a9 -kernel vmlinuz-3.2.0-4-vexpress -initrd initrd.gz -append "root=/dev/ram"  -no-reboot
+#sudo arm-softmmu/qemu-system-arm -m 256M -sd armdisk.img -M versatilepb -kernel vmlinuz-3.2.0-4-versatile  -initrd initrd.img-3.2.0-4-versatile  -append "root=/dev/ram"  -no-reboot
