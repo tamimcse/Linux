6a331229c5abc12748f93dc8262162dedc403589
tamimcse <tamim@csebuet.org>
2017-10-15 18:09:05 -0400

    Updated the topology with 2 hosts and one router

--- smartport.py
+++ smartport.py
@@ -12,7 +12,7 @@ already built into Linux.
 #
 #       172.16.101.0/24         172.16.102.0/24   
 #  h1 ------------------- r1 ------------------ h2
-#    .1                .1   .2               .1   
+#    .1                .2   .3               .1   
 #
 ##############################################################################
 """
@@ -29,25 +29,42 @@ import sys
 import time
 
 
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+
 class NetworkTopo( Topo ):
     def build( self, **_opts ):	
         h1 = self.addHost( 'h1', ip='172.16.101.1/24', defaultRoute='via 172.16.101.2' )
-        h2 = self.addHost( 'h2', ip='172.16.101.2/24', defaultRoute='via 172.16.101.1' )
-	self.addLink(h1, h2)
+        h2 = self.addHost( 'h2', ip='172.16.102.1/24', defaultRoute='via 172.16.102.3' )
+	r1 = self.addNode( 'r1', cls=LinuxRouter, ip='172.16.101.2/24' )
+
+        self.addLink( h1, r1, intfName2='r1-eth2', params2={ 'ip' : '172.16.101.2/24' })
+        self.addLink( h2, r1, intfName2='r1-eth3', params2={ 'ip' : '172.16.102.3/24' })
 
 def main(cli=0):
     "Test linux router"
     topo = NetworkTopo()
 
-    host = custom( CPULimitedHost, cpu=0.5)
-    net = Mininet( topo=topo, host=host, controller = None )
+    net = Mininet( topo=topo, controller = None )
     net.start()
-    net['h2'].cmd( 'tc qdisc add dev h2-eth0 root handle 1: mf')
- #   net['h2'].cmd('iperf -s -p 45678 &')
+
+#    net['r1'].cmd( 'tc qdisc add dev r1-eth3 root handle 1: mf')
+
+#    testing using port 45678 
+#    net['h2'].cmd('iperf -s -p 45678 &')
 #    net['h1'].cmd('iperf -c 172.16.101.2 -p 45678 -t 30')
 
-#    hosts = [net['h1'], net['h2']]        
-#    net.iperf(hosts, seconds=30, l4Type='TCP', udpBw='1000M')
+    hosts = [net['h1'], net['h2']]        
+    net.iperf(hosts, seconds=30, l4Type='UDP', udpBw='850M')
 
     CLI( net )
     net.stop()
@@ -55,7 +72,7 @@ def main(cli=0):
 if __name__ == '__main__':
     args = sys.argv
     setLogLevel( 'info' )
-    cli = 0
-    if "--cli" in args:
-        cli = 1
-    main(cli)
+#    cli = 0
+#    if "--cli" in args:
+#        cli = 1
+    main()
