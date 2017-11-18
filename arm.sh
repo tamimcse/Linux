c0e84276799bbdd97f8fde5497686a04a6db9dbb
tamimcse <tamim@csebuet.org>
2017-11-17 21:16:04 -0500

    .

--- arm.sh
+++ arm.sh
@@ -25,6 +25,7 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  UIMAGE_LOADADDR=0x8000 uImage -j
 
 #ARM emulation
 #./configure --target-list=arm-softmmu,aarch64-softmmu,arm-linux-user --audio-drv-list=alsa,sdl,oss
+#make -j8
 #./qemu-img create -f raw armdisk.img 8G
 #sudo arm-softmmu/qemu-system-arm -m 1024M -sd armdisk.img -M vexpress-a9 -cpu cortex-a9 -kernel vmlinuz-3.2.0-4-vexpress -initrd initrd.gz -append "root=/dev/ram"  -no-reboot
 #sudo arm-softmmu/qemu-system-arm -m 256M -sd armdisk.img -M versatilepb -kernel vmlinuz-3.2.0-4-versatile  -initrd initrd.img-3.2.0-4-versatile  -append "root=/dev/ram"  -no-reboot
@@ -32,6 +33,7 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  UIMAGE_LOADADDR=0x8000 uImage -j
 
 #MIPS emulation
 #./configure --target-list=mips-softmmu,mipsel-softmmu,mips64-softmmu,mips64el-softmmu,mips-linux-user,mipsel-linux-user,mips64-linux-user,mips64el-linux-user,mipsn32-linux-user,mipsn32el-linux-user
+#make -j8
 #sudo mips-softmmu/qemu-system-mips -m 256M -sd armdisk.img -M versatilepb -kernel vmlinuz-3.2.0-4-versatile  -initrd initrd.img-3.2.0-4-versatile  -append "root=/dev/ram"  -no-reboot
 
 
