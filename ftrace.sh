41b2d48f41de047624bb0daa85b93284c7ae960f
tamimcse <tamim@csebuet.org>
2018-03-24 19:36:37 -0400

    .

--- ftrace.sh
+++ ftrace.sh
@@ -1,8 +1,9 @@
+#Enable CONFIG_FUNCTION_TRACER=y
 sudo chmod a+rwx -R /sys/kernel/debug/ &&
 cd /sys/kernel/debug/tracing &&
 cat available_tracers &&
-#echo xdp_router_forward > set_ftrace_filter &&
-#echo function_graph > current_tracer &&
+sudo echo xdp_router_forward ip_route_lookup is_xdp_forwardable > set_ftrace_filter &&
+#echo function > current_tracer &&
 echo function_graph > current_tracer &&
 cat trace
 
