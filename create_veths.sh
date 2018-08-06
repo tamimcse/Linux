85893def65c361aaace91603fae3e99bc510c1c1
tamimcse <tamim@csebuet.org>
2018-08-06 19:54:28 -0400

    added veths to simulate a router

---
+++ create_veths.sh
@@ -0,0 +1,24 @@
+sudo ip link add veth0 type veth peer name veth1
+sudo ip link add veth2 type veth peer name veth3
+sudo ip link add veth4 type veth peer name veth5
+sudo ip link add veth6 type veth peer name veth7
+sudo ip link add veth8 type veth peer name veth9
+sudo ip link add veth10 type veth peer name veth11
+sudo ip link add veth12 type veth peer name veth13
+sudo ip link add veth14 type veth peer name veth15
+sudo ip link set dev veth0 up
+sudo ip link set dev veth1 up
+sudo ip link set dev veth2 up
+sudo ip link set dev veth3 up
+sudo ip link set dev veth4 up
+sudo ip link set dev veth5 up
+sudo ip link set dev veth6 up
+sudo ip link set dev veth7 up
+sudo ip link set dev veth8 up
+sudo ip link set dev veth9 up
+sudo ip link set dev veth10 up
+sudo ip link set dev veth11 up
+sudo ip link set dev veth12 up
+sudo ip link set dev veth13 up
+sudo ip link set dev veth14 up
+sudo ip link set dev veth15 up
