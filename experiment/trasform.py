138a76d8ec1a29bc3f7044471e21701198687556
tamimcse <tamim@csebuet.org>
2018-08-12 14:22:36 -0400

    Removed all the duplicate prefixes from the routes

--- experiment/trasform.py
+++ experiment/trasform.py
@@ -1,11 +1,14 @@
 from subprocess import call
 
-out = open("demofile.txt", "a")
+out = open("demofile1.txt", "a")
 
+ip_list = []
 with open("SAIL_FIB_50K.txt") as f:
     for line in f:
 	arr = line.replace("\r\n", "").split("\t")
-	fib_index = int(arr[1]) % 16
-#    	print "{0}\tveth{1}".format(arr[0], fib_index)
-	out.write("{0}\tveth{1}\r\n".format(arr[0], fib_index))
-#	call('sudo ip route add {0} table main dev {1}'.format(arr[0], arr[1]), shell=True)
+	ip = arr[0].split("/")[0]
+	if ip not in ip_list :
+	    ip_list.append(ip)	
+	    fib_index = int(arr[1]) % 16
+	    out.write("{0}\tveth{1}\r\n".format(arr[0], fib_index))
+
