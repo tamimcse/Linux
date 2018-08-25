d374855fd8e13ff4ede701990177c508475d8af5
tamimcse <tamim@csebuet.org>
2018-08-25 15:31:27 -0400

    Generate veth ID randomly

--- experiment/trasform.py
+++ experiment/trasform.py
@@ -1,10 +1,12 @@
 from subprocess import call
+import random
 
-out = open("demofile1.txt", "a")
+out = open("demofile.txt", "a")
 
 with open("SAIL_FIB.txt") as f:
     for line in f:
 	arr = line.replace("\r\n", "").split("\t")
-        fib_index = int(arr[1]) % 32
+#        fib_index = int(arr[1]) % 32
+        fib_index = random.randint(0,31)
         out.write("{0}\tveth{1}\r\n".format(arr[0], fib_index))
 
