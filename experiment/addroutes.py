61d403c67caea8ed51bafede8e9149c3ea1ef41b
tamimcse <tamim@csebuet.org>
2018-09-03 19:24:25 -0400

    .

--- experiment/addroutes.py
+++ experiment/addroutes.py
@@ -1,6 +1,6 @@
 from subprocess import call
 
-with open("demofile.txt") as f:
+with open("demofile.v2.txt") as f:
     for line in f:
         # Do something with 'line'
 	arr = line.replace("\r\n", "").split("\t")
